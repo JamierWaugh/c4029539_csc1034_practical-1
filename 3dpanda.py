73c1b9f2c6a89ce865007c7207db72f1c1482ef3
Jamie Waugh <jamierwaugh@gmail.com>
2024-09-24 17:35:15 +0100

    Installed panda3d

--- 3dpanda.py
+++ 3dpanda.py
@@ -1,1 +1,9 @@
-print("Hello panda")
+from direct.showbase.ShowBase import ShowBase #Status: Crash
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+app = MyApp()
+
+app.run()
