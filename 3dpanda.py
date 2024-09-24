0cdc0d73796de6059d0de34873009f9f757b7882
Jamie Waugh <jamierwaugh@gmail.com>
2024-09-24 17:47:59 +0100

    Added scenery for panda to live in

--- 3dpanda.py
+++ 3dpanda.py
@@ -1,9 +1,21 @@
-from direct.showbase.ShowBase import ShowBase #Status: Crash
+from direct.showbase.ShowBase import ShowBase
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        #Load environment model
+        self.scene = self.loader.loadModel("models/environment")
+        #Reparent the model to render
+        self.scene.reparentTo(self.render)
+
+        #Apply scale and position transform the model
+
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8,42,0)
+
+
+
 app = MyApp()
 
-app.run()
+app.run() #Starts Panda screen
