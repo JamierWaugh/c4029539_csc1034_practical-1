5c4d5bcfa661ea6ca6a81966d672cd6cbe46b8ad
Jamie Waugh <jamierwaugh@gmail.com>
2024-10-02 13:03:45 +0100

    Added more command line inputs

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -4,7 +4,15 @@ import argparse
 def cliproc():
     parser = argparse.ArgumentParser(prog="walking_panda")
     parser.add_argument("--no-rotate", help = "Suppress Rotation"
-                        , action= "store_true")
+                        , action= "store_true") #No rotate of camera
+    parser.add_argument("--scale",help = "Increase scale"
+                        , action="store_true") #Increase scale of panda
+    parser.add_argument("--no-walk", help = "Suppress Walking"
+                        , action = "store_true") #Stop panda from walking
+    parser.add_argument("--baby-panda", help="Loading second panda"
+                        ,action = "store_true") #Create mother and child panda
+    parser.add_argument("--baby-no-rotate", help="No rotate and baby"
+                        ,action = "store_true") #Baby and no rotate
     args = parser.parse_args()
 
     walking = panda.WalkingPanda(**vars(args))
--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -6,7 +6,7 @@ from math import pi, sin, cos
 #All imports above
 
 class WalkingPanda(ShowBase):
-    def __init__(self, no_rotate=False):
+    def __init__(self, no_rotate=False, scale=False, no_walk=False, baby_panda=False,baby_no_rotate = False):
         ShowBase.__init__(self)
 
         #Load environment model
@@ -20,17 +20,31 @@ class WalkingPanda(ShowBase):
         self.scene.setPos(-8,42,0)
 
         #Add the spinCameraTask procedure to the task manager to spin camera
-        if no_rotate == False:
+        if no_rotate == True or baby_no_rotate == True:
+            self.taskMgr.add(self.noSpinCameraTask, "NoSpinCameraTask")
+        elif no_rotate == False:
             self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-
+        
         #Load and transform the panda actor
         self.pandaActor = Actor("models/panda-model",
-        {"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.005, 0.005, 0.005)
+            {"walk": "models/panda-walk4"})
+        if baby_panda == True or baby_no_rotate == True:
+            self.pandaActor1 = Actor("models/panda-model",
+            {"walk": "models/panda-walk4"})
+            self.pandaActor1.setScale(0.010,0.010,0.010)
+            self.pandaActor1.setPos(-3,8,0)
+            self.pandaActor1.reparentTo(self.render)
+            self.pandaActor1.loop("walk")
+
+        if scale == True:
+            self.taskMgr.add(self.incScale, "incScale")
+        else:
+            self.pandaActor.setScale(0.005, 0.005, 0.005)
         self.pandaActor.reparentTo(self.render) #Renders panda model
 
         #Loop walk
-        self.pandaActor.loop("walk")
+        if no_walk == False:
+            self.pandaActor.loop("walk")
 
     def spinCameraTask(self, task):
         angleDegrees = task.time * 7.0
@@ -38,4 +52,13 @@ class WalkingPanda(ShowBase):
         self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees,0,0)
         return Task.cont
+    
+    def noSpinCameraTask(self, task): #Loads the camera position when camera is not rotating
+        self.camera.setPos(3,-20,3)
+        self.camera.setHpr(15,0,0)
+        return Task.cont
+    
+    def incScale(self,task):
+        self.pandaActor.setScale(0.020, 0.015, 0.015) #increases size of panda
+
 
